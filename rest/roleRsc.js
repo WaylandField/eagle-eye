1f3530877a959d8017d983066fb3dcfac6ba2147
Yichang Tian <tian.yichang@gmail.com>
2013-02-23 16:35:32 +0800

    rest with parameter

--- rest/roleRsc.js
+++ rest/roleRsc.js
@@ -1,7 +1,8 @@
 module.exports = function(app, options){    
     var dao = require('../dao/roleDao');
-    app.get('/api/roles', function(req, res, next){
+    app.get('/api/role/latitude/:latitude/longitude/:longitude/teamview/:teamview', function(req, res, next){
         var latitude = req.params.latitude;
+        console.log(req.params);
         var longitude = req.params.longtitude;
         var teamview = req.params.teamview;
         var query ={latitude:latitude};
