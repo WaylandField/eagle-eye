73381764800e79e66145d20c102dbb9db2ab6b34
unknown <mouyang@MOUYANG.successfactors.com>
2013-02-27 13:40:38 +0800

    Update roleDao.js for generate data.

--- dao/roleDao.js
+++ dao/roleDao.js
@@ -29,7 +29,7 @@ exports.insertRoles=function()
     var roleNameList= new Array("Dev manager", "QA Manager", "QA Lead","Senior QA", "Junior Developer", "Senior Developer","UI Lead","UI desinger","Project manager","DBA");
 	var latitudelist = new Array("40N","51N","25N");
 	var longtitudelist = new Array("80E","10W","120E");
-	var firstNameList= new Array("Bing", "Michael", "Lucy","Kaushik", "Tom", "Hiller","David","Rose","peter","Daniel","lei");
+	var firstNameList= new Array("Bing", "Michael", "Lucy","Kaushik", "Tom", "Hiller","David","Rose","peter","Daniel","lei","Johnson","William","Brown","Miller","Garcia","Martinez","Anderson","Moore","Lopez","Clark","Robinson","Walker","Hall","Allen","Sanchez","Scott","Adams","Campbell","Evans");
 	var lastNameList= new Array("Smith", "roman", "Tian","Yang", "Huang", "Xu","Ronaldo","Rogers","Hu","Liu","Li");
     var compNameList = new Array( "Budgets/Cost Control", "Communication", "Computer Skills","Innovation", "Customer Focus", "People Development","programming","documentation","English","Email");
 	var readinessList = new Array("25%","50%","75%","100%","0%");
@@ -37,17 +37,19 @@ exports.insertRoles=function()
 	var labelList = new Array("complete","Not Ready");
 	var startDateList = new Array("01/02/2006","01/02/2007","01/02/2008","01/02/2009","01/02/2010","01/02/2011","01/02/2012");
 	var endDateList = new Array("01/02/2008","01/02/2008","01/02/2010","01/02/2010","01/02/2011","01/02/2012","01/02/2013");
-    for(var i=0;i<20;i++)
-	{   
+
+    for(var i=0;i<5;i++)
+	{  
+		var j = i+55	
 	    var roleId= "role" + i;
 		var roleName= roleNameList[i%10];
 		var latitude= latitudelist[i%3];
 		var longtitude = longtitudelist[i%3];
-		var userId ="user"+i;
-		var iTemp1 = Math.floor(Math.random() * ( 10 + 1));
+		var userId ="user"+ j;
+		var iTemp1 = Math.floor(Math.random() * ( 29 + 1));
 		var iTemp2 = Math.floor(Math.random() * ( 10 + 1));
 		var userName =firstNameList[iTemp1] + " " + lastNameList[iTemp2];
-		var photo ="resource/photos/" + i +".jpg";
+		var photo ="resource/photos/" + j +".jpg";
 		var compName=compNameList[i%10];
 		var readiness =readinessList[i%5];
 		var learningName =learningList[i%5];
